8ea103514cdb182188ea2f1d13d950626ea68763
kanonlove <70180474+kanonlove@users.noreply.github.com>
2020-09-03 16:52:56 -0500

    Add files via upload

---
+++ IE517_F20_HW2/KNN learning.py
@@ -0,0 +1,123 @@
+import sklearn
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import numpy as np
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
+
+
+
+from sklearn import datasets
+iris = datasets.load_iris()
+X_iris, y_iris = iris.data, iris.target
+
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+X, y = X_iris[:, :2], y_iris
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.25, random_state=15)
+
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+scores=list()
+
+k_range=range(1,26)
+
+for k in k_range:
+    
+    knn=KNeighborsClassifier(n_neighbors=k)
+
+    knn.fit(X_train,y_train)
+
+    y_pred=knn.predict(X_test)
+
+    scores.append(sklearn.metrics.accuracy_score(y_test,y_pred))
+
+
+print(scores)
+
+
+x=range(len(scores))
+
+plt.figure()
+
+plt.scatter(x,scores)
+
+plt.show()
+
+print("the max score is when k=",scores.index(max(scores))+1)
+
+knn=KNeighborsClassifier(n_neighbors=scores.index(max(scores))+1)
+
+knn.fit(X_train,y_train)
+
+y_pred=knn.predict(X_test)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X_combined, y_combined, 
+                      classifier=knn, test_idx=range(105, 150))
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+print("\n")
+
+
+
+print("My name is Yi Zhou")
+print("My NetID is: yizhou16")
+print("I hereby certify that I have read the University policy on Academic Integrity and that I am not in violation.")
+
+    
